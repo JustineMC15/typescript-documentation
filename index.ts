d1cb243f0e59b9a7b983fd791f50e65ffa9c19e6
JustineMC15 <taroyjustine@gmail.com>
2026-05-08 14:50:55 +0800

    Save and Load from file with hardcoded temp values for tasklist

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World");
+import * as fs from 'node:fs'
+import * as readline from 'node:readline'
+import { stdin as input, stdout as output } from 'node:process'
+interface task {
+    taskTitle: string,
+    taskDescription: string,
+    taskDeadline: string
+}
+function loadFromFile(): task[] {
+    if (!fs.existsSync('tasklist.txt')) {
+        console.log("No tasklist data found, starting fresh.")
+        return []
+    }
+    const data = fs.readFileSync('tasklist.txt', 'utf-8')
+    if (data.trim() === '') return [] 
+    return JSON.parse(data)
+}
+const list = loadFromFile()
+console.log(list)
+function saveToFile(list: task[]): void {
+    const data = JSON.stringify(list, null, 2)
+    fs.writeFileSync('tasklist.txt', data)
+    console.log("Data saved successfully.")
+}
